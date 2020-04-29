4e5aae7b81c32b91a3b3aeb91cafdd135073b4a8
natcast <nathalie.brooke@gmail.com>
2020-04-29 13:01:56 +0100

    Some indentation, to make it clean

--- farm.php
+++ farm.php
@@ -65,106 +65,106 @@ $page_title .= ': ' . $pageinfo['Experiment'];
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 
-<head>
-
-<?php
-include 'includes/meta.html'; // that is the <meta and link tags> superseeds head.html
-
-$script = ''; // $script is added to the header as the
-if (isset($datacite)) {
-    $script = "<script type=\"application/ld+json\">" . $datacite . "</script>";
-    echo $script;
-}
-?>
-
-</head>
-
-<body>
-	<div class="container bg-white px-0">
-
+    <head>
+    
+        <?php
+        include 'includes/meta.html'; // that is the <meta and link tags> superseeds head.html
+        
+        $script = ''; // $script is added to the header as the
+        if (isset($datacite)) {
+            $script = "<script type=\"application/ld+json\">" . $datacite . "</script>";
+            echo $script;
+        }
+        ?>
+    
+    </head>
+    
+    <body>
+    	<div class="container bg-white px-0">
+    
+            <?php
+            
+            include 'includes/header.html'; // all the menus at the top
+            
+            // -- start dependant content ---------------------------------------------------------
+            ?>
+            <div id="idExpt">
+            			<h1 class="mx-3"><?php
+                        // /experimentname is found in the datadescription file.
+                        echo $pageinfo['Experiment'];
+                        ?></h1>
+            			<div class="row">
+            				<div class="col-12 pt-3">
+            					<ul class="nav nav-tabs nav-fill text-body ">
+            
+            						<li class="nav-item  active show"><a class="nav-link active"
+            							id="summary-tab" data-toggle="tab" href="#summary">Overview</a></li>
+            						<li class="nav-item"><a class="nav-link" id="history-tab"
+            							data-toggle="tab" href="#history">History</a></li>
+            						<li class="nav-item"><a class="nav-link" id="site-tab"
+            							data-toggle="tab" href="#site">Site</a></li>
+            						<li class="nav-item"><a class="nav-link" id="experiments-tab"
+            							data-toggle="tab" href="#experiments">Experiments</a></li>
+            						<li class="nav-item"><a class="nav-link" id="documents-tab"
+            							data-toggle="tab" href="#documents">Documents</a></li>
+            						<li class="nav-item"><a class="nav-link" id="images-tab"
+            							data-toggle="tab" href="#images">Images</a></li>
+            
+            					</ul>
+            
+            					<div class="tab-content mh-100" id="idExptTabs">
+            						<div class="tab-pane active" id="summary" role="tabpanel"
+            							aria-labelledby="summary-tab">
+            						<?php include ($exptFolder.'/summary.php');?>
+            							</div>
+            
+            						<div class="tab-pane" id="site" role="tabpanel"
+            							aria-labelledby="site-tab"></div>
+            
+            						<div class="tab-pane" id="history" role="tabpanel"
+            							aria-labelledby="history-tab">
+            							<div class="row px-5">
+            						<?php include ($exptFolder.'/history.php');?>	
+            						</div>
+            						</div>
+            
+            						<div class="tab-pane" id="experiments" role="tabpanel"
+            							aria-labelledby="experiments-tab"></div>
+            						<div class="tab-pane" id="images" role="tabpanel"
+            							aria-labelledby="images-tab">
+            							<?php include '_images.php';?>
+            							</div>
+            						<div class="tab-pane" id="documents" role="tabpanel"
+            							aria-labelledby="documents-tab"></div>
+            
+            
+            					</div>
+            				</div>
+            			</div>
+            		</div>
+            					
+            <?php
+            // -- start footers -----------------------------
+            
+            include_once 'includes/footer.html';
+            
+            ?>
+     
+    	</div>
         <?php
         
-        include 'includes/header.html'; // all the menus at the top
-        
-        // -- start dependant content ---------------------------------------------------------
-        ?>
-        <div id="idExpt">
-        			<h1 class="mx-3"><?php
-                    // /experimentname is found in the datadescription file.
-                    echo $pageinfo['Experiment'];
-                    ?></h1>
-        			<div class="row">
-        				<div class="col-12 pt-3">
-        					<ul class="nav nav-tabs nav-fill text-body ">
-        
-        						<li class="nav-item  active show"><a class="nav-link active"
-        							id="summary-tab" data-toggle="tab" href="#summary">Overview</a></li>
-        						<li class="nav-item"><a class="nav-link" id="history-tab"
-        							data-toggle="tab" href="#history">History</a></li>
-        						<li class="nav-item"><a class="nav-link" id="site-tab"
-        							data-toggle="tab" href="#site">Site</a></li>
-        						<li class="nav-item"><a class="nav-link" id="experiments-tab"
-        							data-toggle="tab" href="#experiments">Experiments</a></li>
-        						<li class="nav-item"><a class="nav-link" id="documents-tab"
-        							data-toggle="tab" href="#documents">Documents</a></li>
-        						<li class="nav-item"><a class="nav-link" id="images-tab"
-        							data-toggle="tab" href="#images">Images</a></li>
-        
-        					</ul>
-        
-        					<div class="tab-content mh-100" id="idExptTabs">
-        						<div class="tab-pane active" id="summary" role="tabpanel"
-        							aria-labelledby="summary-tab">
-        						<?php include ($exptFolder.'/summary.php');?>
-        							</div>
-        
-        						<div class="tab-pane" id="site" role="tabpanel"
-        							aria-labelledby="site-tab"></div>
-        
-        						<div class="tab-pane" id="history" role="tabpanel"
-        							aria-labelledby="history-tab">
-        							<div class="row px-5">
-        						<?php include ($exptFolder.'/history.php');?>	
-        						</div>
-        						</div>
-        
-        						<div class="tab-pane" id="experiments" role="tabpanel"
-        							aria-labelledby="experiments-tab"></div>
-        						<div class="tab-pane" id="images" role="tabpanel"
-        							aria-labelledby="images-tab">
-        							<?php include '_images.php';?>
-        							</div>
-        						<div class="tab-pane" id="documents" role="tabpanel"
-        							aria-labelledby="documents-tab"></div>
-        
-        
-        					</div>
-        				</div>
-        			</div>
-        		</div>
-        					
-        <?php
-        // -- start footers -----------------------------
-        
-        include_once 'includes/footer.html';
+        include_once 'includes/finish.inc'; // this has the common js scripts
         
         ?>
- 
-	</div>
-    <?php
-    
-    include_once 'includes/finish.inc'; // this has the common js scripts
-    
-    ?>
-    <script
-    		src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
-    	<script>
-    	      baguetteBox.run('.compact-gallery',{animation:'slideIn',
-    	    	    captions: function(element) {
-    	    	        return element.getElementsByTagName('img')[0].alt;
-    	    	    }});
-    </script>
-</body>
+        <script
+        		src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+        	<script>
+        	      baguetteBox.run('.compact-gallery',{animation:'slideIn',
+        	    	    captions: function(element) {
+        	    	        return element.getElementsByTagName('img')[0].alt;
+        	    	    }});
+        </script>
+    </body>
 
 </html>
 
