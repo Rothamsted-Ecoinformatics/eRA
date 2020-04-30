6d6adad4d0c74cb0b3f438f086b1772df53d2556
natcast <nathalie.brooke@gmail.com>
2020-04-30 13:13:20 +0100

    Made a start on llisting the Rothamsted Experiments

---
+++ metadata/default/experiments.php
@@ -0,0 +1,72 @@
+<?php
+?>
+<h1>All experiments in Database</h1>
+<table><tr><th colspan="5">select e.experiment_name, e.experiment_code, e.purpose, GLTENID is not null AS isLTE , o.organisation_name 
+from experiment e join fields f on f.field_id = e.field_id join organisation o on f.organisation_id = o.organisation_id
+order by o.organisation_name ASC, isLTE desc, e.experiment_name ASC  </th></tr><tr><th>experiment_name</th><th>experiment_code</th><th>purpose</th><th>isLTE</th><th>organisation_name</th></tr><tr class="odd"><td>Brooms Barn Meteorological Station</td><td>BMS</td><td></td><td>0</td><td>Brooms Barn Research Centre</td></tr>
+<tr><td>Acid Strip</td><td>R/RS/9</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Alternate Wheat and Fallow</td><td>R/WF/3</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Amounts of Straw - Rothamsted</td><td>R/CS/326</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Broadbalk</td><td>R/BK/1</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Continuous Maize - Rothamsted</td><td>R/CS/477</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Exhaustion Land</td><td>R/EX/4</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Garden Clover</td><td>R/GC/8</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Geescroft Wilderness</td><td>R/GE/9</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Highfield Bare Fallow</td><td>R/RS/1</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Highfield Conversion</td><td>R/CS/767</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Hoosfield Spring Barley</td><td>R/HB/2</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Park Grass</td><td>R/PG/5</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Rothamsted Long-term Liming</td><td>R/CS/10</td><td></td><td>1</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Agdell</td><td>R/AG/6</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Arable Reference Plots - Rothamsted</td><td>R/RN/5</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Barnfield</td><td>R/BN/7</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Barnfield Mangolds</td><td>X4</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Barnfield Sugarbeet</td><td>X5</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Cereal Disease Reference Plots</td><td>R/RN/9</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Cultivation Weedkiller Rotation Experiment</td><td>R/RN/8</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Deep Cultivation Rotation Experiment</td><td>X8</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Delharding Reserve Site</td><td>R/RS/5</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Factors Limiting Yield - Winder Oilseed Rape</td><td>R/RA/1</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Fallow Reversion Plots - Geescroft</td><td>R/CS/684</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Fallow Reversion Plots - Highfield</td><td>R/CS/683</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Fosters Ley Arable</td><td>R/RN/2</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Four Course Rotation Experiment, Residual Values o</td><td>X3</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Fumigation</td><td>R/RO</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Geescroft Fallow</td><td>R/RS/2</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Grazed Reference Plots</td><td>R/CS/2</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Highfield Ley Arable</td><td>R/RN/1</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Hoosfield Leguminous Plants</td><td>X14</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Intensive Barley Followed by Wheat after Intensive</td><td>R/CS/6</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Irrigation</td><td>R/RN/11</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Levels of N &amp; K</td><td>R/CS/1</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Long Period Cultivation Experiment</td><td>BB/1</td><td>To compare the effects on crop yield and weed infestation of continued ploughing, rotary cultivation, with the Simar implement and stirring the soil with a cultivator.
+
+To investigate the value of cyanamide as a weed-killer.</td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Maize and Nitrogen</td><td>R/MA/1</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Market Garden</td><td>W/RN/4</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>N and Crop Physiology</td><td>R/WW/3</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Nitrogen Levels to Old Grass</td><td>R/CS/13</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Nitrophosphates - Potatoes</td><td>R/P/5</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>NPK to Old Grass</td><td>R/CS/14</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Residual Phosphate</td><td>R/RN/7</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Residual Values Experiment</td><td>X11</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Rothamsted Meteorological Station</td><td>RMS</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Six Course Rotation Experiment - Rothamsted</td><td>X10</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Three Course Rotation Experiment, Effects of Straw</td><td>X2</td><td>To test the long-term effects of raw straw ploughed in and straw compost on a three course rotation.</td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr class="odd"><td>Two Course Rotation Experiment, Effect of Agricult</td><td>X1</td><td></td><td>0</td><td>Rothamsted Experimental Station</td></tr>
+<tr><td>Miscanthus sinensis giganteus study</td><td>R/CS/408</td><td></td><td>1</td><td>Saxmundham Experimental Station</td></tr>
+<tr class="odd"><td>Saxmundham Rotation 1</td><td>S/RN/1</td><td></td><td>1</td><td>Saxmundham Experimental Station</td></tr>
+<tr><td>Saxmundham Rotation 2</td><td>S/RN/2</td><td></td><td>1</td><td>Saxmundham Experimental Station</td></tr>
+<tr class="odd"><td>Saxmundham Meteorological Station</td><td>SMS</td><td></td><td>0</td><td>Saxmundham Experimental Station</td></tr>
+<tr><td>Amounts of Straw - Woburn</td><td>W/CS/326</td><td></td><td>1</td><td>Woburn Experimental Farm</td></tr>
+<tr class="odd"><td>Continuous Maize - Woburn</td><td>W/CS/478</td><td></td><td>1</td><td>Woburn Experimental Farm</td></tr>
+<tr><td>Liquid Sludge Experiment</td><td>W/CS/428</td><td></td><td>1</td><td>Woburn Experimental Farm</td></tr>
+<tr class="odd"><td>Metal Salts Experiment</td><td>W/CS/439</td><td></td><td>1</td><td>Woburn Experimental Farm</td></tr>
+<tr><td>Sewage Cake Experiment</td><td>W/CS/427</td><td></td><td>1</td><td>Woburn Experimental Farm</td></tr>
+<tr class="odd"><td>Continuous Wheat and Barley</td><td>X6</td><td></td><td>0</td><td>Woburn Experimental Farm</td></tr>
+<tr><td>Irrigation Experiment</td><td>X13</td><td></td><td>0</td><td>Woburn Experimental Farm</td></tr>
+<tr class="odd"><td>Rotation and Fumigation</td><td>W/RN/15</td><td></td><td>0</td><td>Woburn Experimental Farm</td></tr>
+<tr><td>Woburn Ley Arable</td><td>W/RN/3</td><td></td><td>0</td><td>Woburn Experimental Farm</td></tr>
+<tr class="odd"><td>Woburn Long-term Liming</td><td>W/CS/10</td><td></td><td>0</td><td>Woburn Experimental Farm</td></tr>
+<tr><td>Woburn Meteorological Station</td><td>WMS</td><td></td><td>0</td><td>Woburn Experimental Farm</td></tr>
+</table>
--- metadata/rothamsted/experiments.php
+++ metadata/rothamsted/experiments.php
@@ -1,3 +1,255 @@
 <?php
 ?>
-<h1>Experiments</h1>
+<h2>Experiments at Rothamsted Site</h2>
+
+<h3 class="mt-3">Classical Experiments</h3>
+
+
+<table>
+	<tr>
+		<th>experiment_name</th>
+		<th>experiment_code</th>
+		<th>purpose</th>
+		<th>Started</th>
+	</tr>
+
+	<tr class="odd">
+		<td>Broadbalk</td>
+		<td><a href="expt.php?expt=rbk1">R/BK/1</a></td>
+		<td>To test the effects of various combinations of inorganic fertilizerand  organic manures on the yield of <b> winter wheat</b></td>
+		<td>1843</td>
+	</tr>
+	<tr class="odd">
+		<td>Park Grass</td>
+		<td><a href="expt.php?expt=rpg5">R/PG/5</td>
+		<td>To investigate ways of improving the yield of <b> hay </b>by the application of inorganic fertilizers and organic manure</td>
+		<td>1856</td>
+	</tr>
+	<tr>
+		<td>Hoosfield Spring Barley</td>
+		<td><a href="expt.php?expt=rhb2">R/HB/2</td>
+		<td>To test the effects of different inorganic fertilizers and farmyard manure  on the yield of <b>spring barley</b></td>
+		<td>1852</td>
+	</tr>
+	<tr class="odd">
+		<td>Alternate Wheat and Fallow</td>
+		<td><a href="expt.php?expt=rwf3">R/WF/3</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+	<tr class="odd">
+		<td>Exhaustion Land</td>
+		<td><a href="expt.php?expt=rex4">R/EX/4</td>
+		<td>Observe the residual effects of mineral fertilizer and manures on the yield of spring barley and winter wheat</td>
+		<td>1</td>
+	</tr>
+	<tr class="odd">
+		<td>Ley Arable</td>
+		<td><a href="expt.php?expt=rrn1">R/RN/1</td>
+		<td>Effects of continuous arable and ley arable cropping systems on soil organic matter and fertility</td>
+		<td>1</td>
+	</tr>
+	<tr class="odd">
+		<td>Geescroft Wilderness</td>
+		<td><a href="expt.php?expt=rge9">R/GE/9</td>
+		<td>Left to revert naturally to <b>woodland</b>.</td>
+		<td>1</td>
+	</tr>
+</table>
+
+<h3 class="mt-3">Other Long-term Experiments</h3>
+
+<table>
+	<tr>
+		<th>experiment_name</th>
+		<th>experiment_code</th>
+		<th>purpose</th>
+		<th>isLTE</th>
+	</tr>
+	<tr>
+		<td>Amounts of Straw</td>
+		<td><a href="expt.php?expt=rcs326">R/CS/326</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+
+	<tr>
+		<td>Continuous Maize</td>
+		<td><a href="expt.php?expt=rcs477">R/CS/477</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+
+	<tr>
+		<td>Garden Clover</td>
+		<td><a href="expt.php?expt=rgc8">R/GC/8</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+
+	<tr>
+		<td>Highfield Bare Fallow</td>
+		<td><a href="expt.php?expt=rrs1">R/RS/1</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+	<tr class="odd">
+		<td>Highfield Conversion</td>
+		<td><a href="expt.php?expt=rcs767">R/CS/767</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+	<tr>
+		<td>Rothamsted Long-term Liming</td>
+		<td><a href="expt.php?expt=rcs10">R/CS/10</td>
+		<td></td>
+		<td>1</td>
+	</tr>
+
+
+</table>
+
+<h3  class="mt-3">Others</h3>
+<p>These are in the database, but not in GLTEN</p>
+
+<table>
+	<tr>
+		<th>experiment_name</th>
+		<th>experiment_code</th>
+		<th>purpose</th>
+		<th>isLTE</th>
+	</tr>
+	<tr class="odd">
+		<td>Agdell</td>
+		<td>R/AG/6</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Arable Reference Plots - Rothamsted</td>
+		<td>R/RN/5</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Barnfield</td>
+		<td>R/BN/7</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Barnfield Mangolds</td>
+		<td>X4</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Barnfield Sugarbeet</td>
+		<td>X5</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Cereal Disease Reference Plots</td>
+		<td>R/RN/9</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Cultivation Weedkiller Rotation Experiment</td>
+		<td>R/RN/8</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Deep Cultivation Rotation Experiment</td>
+		<td>X8</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Delharding Reserve Site</td>
+		<td>R/RS/5</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Factors Limiting Yield - Winder Oilseed Rape</td>
+		<td>R/RA/1</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Fallow Reversion Plots - Geescroft</td>
+		<td>R/CS/684</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Fallow Reversion Plots - Highfield</td>
+		<td>R/CS/683</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Fosters Ley Arable</td>
+		<td>R/RN/2</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Four Course Rotation Experiment, Residual Values o</td>
+		<td>X3</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Fumigation</td>
+		<td>R/RO</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Geescroft Fallow</td>
+		<td>R/RS/2</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Grazed Reference Plots</td>
+		<td>R/CS/2</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Highfield Ley Arable</td>
+		<td>R/RN/1</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Hoosfield Leguminous Plants</td>
+		<td>X14</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Intensive Barley Followed by Wheat after Intensive</td>
+		<td>R/CS/6</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr class="odd">
+		<td>Irrigation</td>
+		<td>R/RN/11</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>Levels of N &amp; K</td>
+		<td>R/CS/1</td>
+		<td></td>
+		<td>0</td>
+	</tr>
+
+</table>
