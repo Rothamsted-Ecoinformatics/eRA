2fc12a3fd52e6922de6eb2ad63bc2559dcd2a7b1
natcast <nathalie.brooke@gmail.com>
2020-05-04 11:00:30 +0100

    Placeholders for bms

---
+++ metadata/bms/measurements.php
@@ -0,0 +1,177 @@
+<?php
+?>
+<div class="row mx-3">
+	<h2>Rothamsted Weather data available in ROTHMET</h2>
+
+	<p>
+		Daily meteorological data recorded at Rothamsted, Harpenden,
+		Hertfordshire available from e-RA. Hourly data for Rothamsted from
+		2004 is available from the <a href="mailto:era@rothamsted.ac.uk"> e-RA
+			Curators.</a>
+	</p>
+	<div class="table-responsive-sm">
+		<table
+			class="table  table-responsive-sm table-sm  table-hover table-bordered">
+			<thead>
+
+				<th scope="col">Variable</th>
+				<th scope="col">Units</th>
+				<th scope="col">Description</th>
+				<th scope="col">How is it measured?</th>
+			</thead>
+			<tbody>
+				<tr name="title_row">
+					<td colspan="4"><h3>Air temperature</h3></td>
+				</tr>
+				<tr>
+					<td>TMAX</td>
+					<td>degrees C</td>
+
+					<td>Maximum temperature, 1982-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>TMIN</td>
+					<td>degrees C</td>
+
+					<td>Minimum temperature, 1982-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>WETB</td>
+					<td>degrees C</td>
+
+					<td>Wet bulb temperature, 1982 - 2009</td>
+				<td></td></tr>
+				<tr>
+					<td>DRYB</td>
+					<td>degrees C</td>
+
+					<td>Dry bulb Temperature, 1982-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>DEWP</td>
+					<td>degrees C</td>
+
+					<td>Dew point (derived from DRYB and WETB), 1982-2009</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Soil temperature under grass</h3></td>
+				</tr>
+				<tr>
+					<td>GRSMIN</td>
+					<td>degrees C</td>
+
+					<td>Grass minimum temperature, 1982-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>G30T</td>
+					<td>degrees C</td>
+
+					<td>Soil temperature under grass at 30cm, 2012-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>G50T</td>
+					<td>degrees C</td>
+
+					<td>Soil temperature under grass at 50cm, 2012-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>G100T</td>
+					<td>degrees C</td>
+
+					<td>Soil temperature under grass at 100cm, 2012-current date</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Soil temperature under bare soil</h3></td>
+				</tr>
+				<tr>
+					<td>S10T</td>
+					<td>degrees C</td>
+
+					<td>Bare soil temperature at 10cm, 2012-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>S20T</td>
+					<td>degrees C</td>
+
+					<td>Bare soil temperature at 20cm, 1982-current date</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Rainfall</h3></td>
+				</tr>
+				<tr>
+					<td>RAIN</td>
+					<td>mm</td>
+
+					<td>Rainfall, 1982-current date</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Sunshine</h3></td>
+				</tr>
+				<tr>
+					<td>SUN</td>
+					<td>hr</td>
+
+					<td>Hours of sunshine, 1982-current date</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Radiation</h3></td>
+				</tr>
+				<tr>
+					<td>RAD</td>
+					<td>MJ/m2</td>
+
+					<td>Total radiation, 1982-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>NETRAD</td>
+					<td>MJ/m2</td>
+
+					<td>Net radiation, 1997-current date</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Wind</h3></td>
+				</tr>
+				<tr>
+					<td>WINDRUN</td>
+					<td>km</td>
+
+					<td>Run of Wind 0900 to 0900 GMT, 1982-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>WINDSP</td>
+					<td>m/s</td>
+
+					<td>Wind speed at 0900 GMT (at 10m), 2012-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>WDIR</td>
+					<td>degrees</td>
+
+					<td>Wind direction at 9.00 GMT (0-360 degrees), 2012-current date</td>
+				<td></td></tr>
+				<tr name="title_row">
+					<td colspan="4"><h3>Other</h3></td>
+				</tr>
+				<tr>
+					<td>GROUND</td>
+					<td></td>
+
+					<td>State of ground (code), 1982-96</td>
+				<td></td></tr>
+				<tr>
+					<td>RELH</td>
+					<td>%</td>
+
+					<td>Relative humidity at 0900 GMT, 2009-current date</td>
+				<td></td></tr>
+				<tr>
+					<td>VAP</td>
+					<td>mb</td>
+
+					<td>Vapour pressure (derived from DRYB and WETB), 1982-2009</td>
+				<td></td></tr>
+			</tbody>
+		</table>
+	</div>
+
+</div>
