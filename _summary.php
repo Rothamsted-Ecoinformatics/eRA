01ac8746d8a5f908ae65e9b36ec182c7989dcc3c
natcast <nathalie.brooke@gmail.com>
2020-05-05 10:56:01 +0100

    corrected license. adde logo, added funders

--- _summary.php
+++ _summary.php
@@ -33,13 +33,12 @@ function getPersonInfo($personDetails)
         $line .= "\n<li class=\"list-group-item \"><h4 class=\"mt-3\">" . $personDetails['givenName'] . "  " . $personDetails['familyName'] . "</h4></li>";
     }
     if ($personDetails['jobTitle']) {
-        $line .= "\n<li class=\"list-group-item pl-5\"><b>Role: </b>". title_case($personDetails['jobTitle']) . "</li>";
+        $line .= "\n<li class=\"list-group-item pl-5\"><b>Role: </b>" . title_case($personDetails['jobTitle']) . "</li>";
     }
     if ($personDetails['sameAs']) {
         $line .= "\n<li class=\"list-group-item pl-5\"><b>ORCID: </b><a href=\"" . $personDetails['sameAs'] . "\">" . $personDetails['sameAs'] . "</a>" . "</li>";
     }
 
-    
     if ($personDetails['affiliation']['type'] == "Organization") {
         $line .= "\n<li class=\"list-group-item pl-5\"><b>Organisation: </b>" . $personDetails['affiliation']['name'] . "</li>";
         $line .= "\n<li class=\"list-group-item pl-5\"><b>Address: </b>" . $personDetails['affiliation']['address'] . "</li>";
@@ -52,30 +51,30 @@ function getPersonInfo($personDetails)
 
 <h2 class="mx-3">Overview</h2>
 <div class="mx-5">
-	
+
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<ul class="list-group mx-3">
-					<?php 
-					$line = '';
-					if ($experiment['administrative']['localIdentifier']) {
-					    $line .= "<li class=\"list-group-item \" ><b>Experiment Code: </b>";
-                        $line .= $experiment['administrative']['localIdentifier'];
-                        $line .= "</li>";
-                       }
-					     if ($experiment['administrative']['description']) {
-					    $line .= "<li class=\"list-group-item\"  style=\"white-space: pre-wrap;\" ><b>Description: </b>";
-                        $line .= $experiment['administrative']['description'];
-                        $line .= "</li>";
-                       }
-                       if ($experiment['administrative']['disambiguatingDescription']) {
-                           $line .= "<li class=\"list-group-item\"  style=\"white-space: pre-wrap;\" ><b>Goals: </b>";
-                           $line .= $experiment['administrative']['disambiguatingDescription'];
-                           $line .= "</li>";
-                       }
-                       echo $line;
-					    ?>
+				<ul class="list-group">
+					<?php
+    $line = '';
+    if ($experiment['administrative']['localIdentifier']) {
+        $line .= "<li class=\"list-group-item \" ><b>Experiment Code: </b>";
+        $line .= $experiment['administrative']['localIdentifier'];
+        $line .= "</li>";
+    }
+    if ($experiment['administrative']['description']) {
+        $line .= "<li class=\"list-group-item\"  style=\"white-space: pre-wrap;\" ><b>Description: </b>";
+        $line .= $experiment['administrative']['description'];
+        $line .= "</li>";
+    }
+    if ($experiment['administrative']['disambiguatingDescription']) {
+        $line .= "<li class=\"list-group-item\"  style=\"white-space: pre-wrap;\" ><b>Goals: </b>";
+        $line .= $experiment['administrative']['disambiguatingDescription'];
+        $line .= "</li>";
+    }
+    echo $line;
+    ?>
 					
 				
 
@@ -86,7 +85,7 @@ function getPersonInfo($personDetails)
             	<?php } ?>
             	<li class="list-group-item "><b>Date End: </b>  <?php if (!$experiment['dateEnd']) {echo"Ongoing";} else { echo $experiment['dateEnd'];}; ?></li>
 
-		
+
 				</ul>
 			</div>
 			<div class="col">
@@ -122,36 +121,57 @@ function getPersonInfo($personDetails)
 	</div>
 	
 	
-          <?php 
-//              if ( $hasTimeline ){ 
-//             echo("     <h3 class=\"my-3 mt-5\">Timeline</h3>");
-//             include '_timeline.php';
-//              } else {; } 
-            
-            
-            ?> 
+          <?php
+        // if ( $hasTimeline ){
+        // echo(" <h3 class=\"my-3 mt-5\">Timeline</h3>");
+        // include '_timeline.php';
+        // } else {; }
+
+        ?> 
 							  
             
     <h3 class="my-3 mt-5">Data Access</h3>
-       <?php
-    $list = listAttributes2($experiment['dataAccess']);
-    echo $list;
-    ?>
-    <h3 class="my-3 mt-5">License</h3>
+       	<?php
+        $list = listAttributes2($experiment['dataAccess']);
+        // echo $list;
+        ?>
+      
+       	<ul class="list-group mx-3">
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>Type:</b>
+			creativeWork</li>
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>Condition
+				of Access:</b> Waiting for Boiler plate text</li>
+
+	</ul>
+
+
+	<h3 class="my-3 mt-5">License</h3>
             <?php
             $list = listAttributes2($experiment['license']);
-            echo $list;
-
-            if (count($person['contributors']) > 0) {
-
-                $line = "<h3 class=\"my-3 mt-5\">Contributors</h3> <ul class=\"list-group  mx-3\">";
-                echo $line;
-                foreach ($person['contributors'] as $personDetails) {
-                    $list = getPersonInfo($personDetails);
-                    echo $list;
-                }
-                echo "</ul>";
-            }
+            // echo $list;
             ?>
-               
+ 	<ul class="list-group mx-3">
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>Type: </b>CreativeWork</li>
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>Title: </b>CC0</li>
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>License: </b><a href="https://creativecommons.org/share-your-work/public-domain/cc0/"><img src="images/logos/cc0.png"></a></li>
+	</ul>
+<?php
+if (count($person['contributors']) > 0) {
+
+    $line = "<h3 class=\"my-3 mt-5\">Contributors</h3> <ul class=\"list-group  mx-3\">";
+    echo $line;
+    foreach ($person['contributors'] as $personDetails) {
+        $list = getPersonInfo($personDetails);
+        echo $list;
+    }
+    echo "</ul>";
+}
+?>
+               	<h3 class="my-3 mt-5">Funding</h3>
+         
+ 	<ul class="list-group mx-3">
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>BBSRC / UKRI: </b>National Capability</li>
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>Grant Number: </b></li>
+		<li class="list-group-item " style="white-space: pre-wrap;"><b>Please update: </b></li>
+	</ul>
     </div>
