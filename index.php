056c32be6f2dce88e6657a9fb97ec49814199f9e
natcast <nathalie.brooke@gmail.com>
2020-05-12 12:57:30 +0100

    refactor index.php

--- index.php
+++ index.php
@@ -1,40 +1,44 @@
-<?php 
+<?php
 include_once 'includes/init.inc'; // these are the settings that refer to more than one page
 
 $page_title .= "";
 
 ?>
-<?php include 'includes/head.html';     // that is the <head tags>?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+	<head>  
+        <?php
+        include 'includes/meta.html'; // that is the <meta and link tags> superseeds head.html
+
+        $script = ''; // $script is added to the header as the
+        if (isset($datacite)) {
+            $script = "<script type=\"application/ld+json\">" . $datacite . "</script>";
+            echo $script;
+        }
+        ?>  
+    </head>
 <body>
-    
-<div class="container bg-white px-0">
 
-<?php include 'includes/header.html';   // all the menus at the top 
+	<div class="container bg-white px-0">
 
-//--  start dependant content ---------------------------------------------------------
-if (!isset($incPage)) {
-include '_carousel.php';
-include '_about.php';
-include '_tools.php'; 
-include '_people.php';
-} 
-else 
-{   
-    $incPage = '_'.$incPage.'.php';    
-    include $incPage;
-    
-}
-	
-//-- start footers  -----------------------------
-
-include_once 'includes/footer.html';
-include_once 'includes/finish.inc';
-
-
+            <?php
+            
+            include 'includes/header.html'; // all the menus at the top
+            
+            // -- start dependant content ---------------------------------------------------------
+            
+            include '_carousel.php';
+            include '_about.php';
+            include '_tools.php';
+            include '_people.php';
+            
+            
+            // -- start footers -----------------------------
+           
+            include_once 'includes/footer.html';
+            include_once 'includes/finish.inc';
 ?>
-
- 
-</div>
+	</div>
 </body>
 
 </html>
