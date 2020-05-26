6a940b537e863a055177245f71fc581cb7dfd067
natcast <nathalie.brooke@gmail.com>
2020-05-26 12:24:17 +0100

    removed old dataset tab from expt

--- expt.php
+++ expt.php
@@ -109,14 +109,10 @@ echo title_case($experiment['administrative']['name']);
 						<li class="nav-item"><a class="nav-link" id="design-tab"
 							data-toggle="tab" href="#design">Design</a></li>
                 						
-                						<?php if ($hasDatasets) {?>
-                						<li class="nav-item"><a class="nav-link"
-							id="datasets-tab" data-toggle="tab" href="#datasets">Datasets</a></li>
-                						<?php } ?>
+                						
                 		<?php if ($hasDatasets) {?>
                 						<li class="nav-item"><a class="nav-link"
-							id="datasets2-tab" data-toggle="tab" href="#datasets2">Datasets
-								Cards</a></li>
+							id="datasets-tab" data-toggle="tab" href="#datasets">Datasets</a></li>
                 						<?php } ?>
                 						<li class="nav-item"><a class="nav-link"
 							id="images-tab" data-toggle="tab" href="#images">Images</a></li>
@@ -147,17 +143,12 @@ echo title_case($experiment['administrative']['name']);
                 							<?php include '_site.php';?>
                 							</div>
                 								<?php if ($hasDatasets) {?>
-                						<div class="tab-pane  pb-3" id="datasets2"
-							role="tabpanel" aria-labelledby="datasets2-tab">
+                						<div class="tab-pane  pb-3" id="datasets"
+							role="tabpanel" aria-labelledby="datasets-tab">
                 							<?php include '_datasetsCards.php';?>
                 							</div>
                 							<?php }?>
-                							<?php if ($hasDatasets) {?>
-                						<div class="tab-pane  pb-3" id="datasets"
-							role="tabpanel" aria-labelledby="datasets-tab">
-                							<?php include '_datasets.php';?>
-                							</div>
-                							<?php }?>
+                							
                 						<div class="tab-pane" id="images" role="tabpanel"
 							aria-labelledby="images-tab">
                 							<?php //include '_images.php';?>
