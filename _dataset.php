a93efc9f7cb507c046ad13449e65afea315a8ac8
natcast <nathalie.brooke@gmail.com>
2020-07-30 13:18:41 +0100

    modal title

--- _dataset.php
+++ _dataset.php
@@ -370,7 +370,7 @@ if (isset($arrDescription['Other'])) {
 	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+				<h5 class="modal-title" id="exampleModalLongTitle">End User Agreement</h5>
 				<button type="button" class="close" data-dismiss="modal"
 					aria-label="Close">
 					<span aria-hidden="true">&times;</span>
