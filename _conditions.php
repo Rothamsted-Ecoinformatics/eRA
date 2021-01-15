44a929d70c05a4a6a7e388965197c7490ced695a
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2021-01-15 18:07:12 +0000

    Minor corrections to new pages HTML

--- _conditions.php
+++ _conditions.php
@@ -5,15 +5,15 @@
 	<div class="row p-3 m-3">
 		<p>This dataset is available under a <a href="https://creativecommons.org/licenses/by/4.0/legalcode">Creative Commons Attribution Licence (4.0)</a> with the following conditions of re-use:</p>
 		<ol>
-		<li>Where the data is presented, either in original or derived form, such as a graphic or summary statistics, you must always use the following attribution statement to acknowledge the source of the information:
-			<blockquote>Contains data supplied by Rothamsted Research.</blockquote>
+		<li class="mb-1">Where the data is presented, either in original or derived form, such as a graphic or summary statistics, you must always use the following attribution statement to acknowledge the source of the information:
+			<i>Contains data supplied by Rothamsted Research.</i>
 		</li>
-		<li>You must ensure the dataset is correctly cited and includes the dataset DOI, either where it is presented or in the reference list of any publications, reports or other outputs which use this dataset in whole or in part. Correct citation of datasets helps us to monitor their use and report their continuing impact to funders.</li>
-		<li>If you publish using data or material from LTE datasets, in addition to following the data citation guidelines above, we also request you notify us of the publication.</li>
-<li>If you require significant support from Rothamsted Scientists to compile, understand or analyse a dataset we  request co-authorship on publications using the data.</li>
-<li>Any reasonable changes made to the dataset must be indicated but not in a way that suggests Rothamsted Research endorses the change or use.</li>
-<li>Rothamsted Research may publish corrected or updated versions of the dataset without notice. All published datasets are version controlled. </li>
-<li>You must not intentionally misrepresent the dataset.  If you have any doubts about interpretation of the data please contact the e-RA curators and they will be happy to support you.</li>
+		<li class="mb-2">You must ensure the dataset is correctly cited and includes the dataset DOI, either where it is presented or in the reference list of any publications, reports or other outputs which use this dataset in whole or in part. Correct citation of datasets helps us to monitor their use and report their continuing impact to funders.</li>
+		<li class="mb-2">If you publish using data or material from LTE datasets, in addition to following the data citation guidelines above, we also request you notify us of the publication.</li>
+<li class="mb-1">If you require significant support from Rothamsted Scientists to compile, understand or analyse a dataset we  request co-authorship on publications using the data.</li>
+<li class="mb-1">Any reasonable changes made to the dataset must be indicated but not in a way that suggests Rothamsted Research endorses the change or use.</li>
+<li class="mb-1">Rothamsted Research may publish corrected or updated versions of the dataset without notice. All published datasets are version controlled. </li>
+<li class="mb-1">You must not intentionally misrepresent the dataset.  If you have any doubts about interpretation of the data please contact the e-RA curators and they will be happy to support you.</li>
 		</ol>
 		<p>If you are uncertain how best to use the dataset or do not understand some aspect of the dataset, the accompanying metadata and supporting information or conditions of use, you are strongly advised to contact the <a href="mailto:era@rothamsted.ac.uk" target="_blank">e-RA Data Curators</a> for support. </p>
 
--- _howtoaccessdata.php
+++ _howtoaccessdata.php
@@ -9,11 +9,14 @@
 		<li>Published annual plot datasets and daily met station datasets</li>
 		<li>Request only access to the e-RA database</li>
 		</ul>
+	<div class="row p-3 m-3">
 		<h2>Published aggregated datasets</h2>
 <p>Aggregated datasets provide access to datasets for which annual plot data has been averaged across a number of years or plots such that data values from an individual plot
  in a year cannot be determined. Aggregated datasets provide a simplified or summary overview of an LTE dataset and key findings or changes of an LTE. For meteorological datasets, aggregated datasets average hourly and daily data to monthly or annual values.</p>
 <p>Aggregated datasets are published with a citable DOI and are freely available under a CC-BY-4.0 license. Users of these datasets must give appropriate credit by citing the dataset using the provided citation which includes the DOI. Any reasonable changes made to the dataset must be indicated but not in a way that suggests Rothamsted Research endorses the user or the use. </p>
-<p>Before downloading a dataset you should read the <a href="info.php?FileName=conditions">Conditions of Use</a> and must agree to these conditions.</p>
+<p>Before downloading a dataset you should read the <a href="conditions.php">Conditions of Use</a> and must agree to these conditions.</p>
+</div>
+<div class="row p-3 m-3">
 <h2>Published annual plot datasets and daily met station datasets</h2>
 <p>Annual plot datasets provide access to observed and measured data values from individual plots for each year data has been collected. Meteorological datasets are provided at daily and hourly resolutions.</p>
 <p>To access these datasets we ask you to complete a simple registration form and provide a valid email. We ask you to complete this for two reasons:</p>
@@ -31,14 +34,16 @@
 </ol>
 <p>The data not included in these datasets is accessible from the e-RA database and can be accessed on request.</p>
 <p>Annual plot datasets datasets are published with a citable DOI and are freely available under CC-BY-4.0 license. Users of these datasets must give appropriate credit by citing the dataset using the provided citation which includes the DOI. Any reasonable changes made to the datasets must be indicated but not in a way that suggests Rothamsted Research endorses the user or the use.</p>
-<p>Before downloading a dataset you should read the <a href="info.php?FileName=conditions">Conditions of Use</a> and must agree to these conditions.</p>
-<h2>Request only access to the e-RA database.<h2> 
+<p>Before downloading a dataset you should read the <a href="conditions.php">Conditions of Use</a> and must agree to these conditions.</p>
+</div>
+<div class="row p-3 m-3">
+<h2>Request only access to the e-RA database.</h2> 
 <p>The e-RA database provides access to additional curated LTE data. The database is accessible online using the password protected e-RA data extraction tool. Normally access will be provided for researchers requesting access to data in specific datasets not included in published datasets. </p>
 <p>We do not publish all data for a number of reasons including:</p>
 <ul>
 <li>A plot does not have continuity of treatments over time.</li>
-<li>The treatments do not have relevance or comparability to other experiment treatments.<li>
-<li>The data is not well enough described or is still in preparation.<li>
+<li>The treatments do not have relevance or comparability to other experiment treatments.</li>
+<li>The data is not well enough described or is still in preparation.</li>
 </ul>
 <p>Researchers are strongly encouraged to seek support from the <a href="mailto:era@rothamsted.ac.uk" target="_blank">e-RA Data Curators</a> to interpret this data.</p>
 
